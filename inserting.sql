c4d919041f57486eb11b820d06aeef50f86cc72a
mohamedabdiahmed <mohamedabdikaniahmed@gmail.com>
2025-03-12 14:27:44 +0300

    inserting.sql

---
+++ inserting.sql
@@ -0,0 +1,13 @@
+-- single row insert
+insert into students(id,name,address)
+values(1,'Rahma abdalle','Siinay');
+
+-- multiple rows insert
+insert into students(id,name,address)
+values
+  (2,'aamiin cismaan','daruuraha'),
+  (3,'khadra xasan','calaamadaha'),
+  (4,'warda xasan','xawaadle')
+
+-- insert without specifiying columns
+insert into students values(5,'xasan aadan','jigjiga yar')
