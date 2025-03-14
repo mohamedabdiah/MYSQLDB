3ac6d5986fffe223aca17da277080a1693034f50
mohamedabdiahmed <mohamedabdikaniahmed@gmail.com>
2025-03-14 14:14:41 +0300

    Create assignment.sql

---
+++ assignment.sql
@@ -0,0 +1,17 @@
+-waxaad soo samaysa database magaciisu yahay Dawlada
+- waxad usoo samaysaa 3 table oo kala ah 
+ 1. xafiiska dakhliga
+ 2. xafiiska waxrbashada
+ 3. xafiiska caafimaadka
+- midkasta oo kamdi ah tables kaas waxaad kusoo diwaan gelinaysaa
+50 data table kastaana haaka koobnaado 6 column 
+tusaale ahaan
+- xafiiska dakhliga 
+ 1. id
+ 2. name 
+ 3. department
+ 4. salary
+ 5. nationalID
+ 6. sex
+- waxaa laga rabaa inaad isku join garayso dhamaan tables kaas 
+adoo kasoo saari dona kadib qofka caaglaha ah 
